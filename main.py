83796c6d95c0f412d8460aa6338a5de209670c1d
laiso <aloisleroy2016@gmail.com>
2024-09-28 21:36:00 +0200

    Adding try catch to code execution

--- main.py
+++ main.py
@@ -20,8 +20,7 @@ def main() -> None:
     
     parser.add_argument("-a", "--admin", type=str, help="Wordlist for the admin pages")
     parser.add_argument("-s", "--subs", type=str, help="Wordlist for subdomain enumeration")
-
-    parser.add_argument("-c", "--concurrency", type=int, help="Set the max concurrency for the async function")
+    parser.add_argument("-c", "--concurrency", type=int, default=10, help="Set the max concurrency for the async function")
     parser.add_argument('-v', '--verbose', action="store_true", default=False, help="Add verbose to the output")
 
     args = parser.parse_args()
@@ -36,28 +35,28 @@ def main() -> None:
     if not os.path.exists("output/"):
         os.makedirs("output")
 
-    print(f"{Fore.BLUE}[i] Info: Gathering robots.txt{Fore.RESET}")
-    get_robots(domain, verbose)
+    try:
+        print(f"{Fore.BLUE}[i] Info: Gathering robots.txt{Fore.RESET}")
+        get_robots(domain, verbose)
 
-    print(f"{Fore.BLUE}[i] Info: Gathering sitemap.xml{Fore.RESET}")
-    get_sitemap(domain, verbose)
+        print(f"{Fore.BLUE}[i] Info: Gathering sitemap.xml{Fore.RESET}")
+        get_sitemap(domain, verbose)
 
-    if adminList:
-        print(f"{Fore.BLUE}[i] Info: Gathering admin pages{Fore.RESET}")
-        if maxConcurrency:
+        if adminList:
+            print(f"{Fore.BLUE}[i] Info: Gathering admin pages{Fore.RESET}")
             asyncio.run(get_admin_page(domain, adminList, verbose, maxConcurrency))
-        else: 
-            asyncio.run(get_admin_page(domain, adminList, verbose))
 
-    print(f"{Fore.BLUE}[i] Info: Querying DNS{Fore.RESET}")
-    dns_query(domain, verbose)
+        print(f"{Fore.BLUE}[i] Info: Querying DNS{Fore.RESET}")
+        dns_query(domain, verbose)
 
-    if subdomainList:
-        print(f"{Fore.BLUE}[i] Info: Searching for subdomains{Fore.RESET}")
-        if maxConcurrency:
+        if subdomainList:
+            print(f"{Fore.BLUE}[i] Info: Searching for subdomains{Fore.RESET}")
             asyncio.run(get_subdomain(domain, subdomainList, verbose, maxConcurrency))
-        else:
-            asyncio.run(get_subdomain(domain, subdomainList, verbose))
+
+    except KeyboardInterrupt:
+        print(f"{Fore.YELLOW}[!] Keyboard interruption detected, quitting...{Fore.RESET}")
+    except Exception as e:
+        print(f"{Fore.RED}[!] Error: An error occured:\n{e}\n{Fore.RESET}")
 
 if __name__ == "__main__":
     main()
